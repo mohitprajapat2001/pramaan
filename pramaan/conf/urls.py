25bc56d25bc0e0f8caef4a8cd2b66802d2f28fd3
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2025-04-08 15:47:13 +0530

    Static Api Added

--- pramaan/conf/urls.py
+++ pramaan/conf/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from conf.routers import router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/v1/",
+        include(router.urls),
+    ),
 ]
